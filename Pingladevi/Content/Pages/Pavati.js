e870dba4d1d0c3d028eeb5bfdecca7d1ba5fa438
krutikawase1997@outlook.com <krutikawase1997@outlook.com>
2021-12-18 22:40:57 +0530

    Add project files.

---
+++ Pingladevi/Content/Pages/Pavati.js
@@ -0,0 +1,78 @@
+﻿$(document).ready(function () {
+     /*getMainPavatiList();*/
+});
+var savePavati = function () {
+    var id = $("#txtId").val();
+    var date = $("#txtDate").val();
+    var name = $("#txtName").val();
+    var mobileno = $("#txtMobileno").val();
+    var paymentinword = $("#txtPaymentInWord").val();
+    var paymentinno = $("#txtPaymentInNo").val();
+    
+    var model = {
+        Id: id,
+        Date: date,
+        Name: name,
+        Mobileno: mobileno,
+        PaymentInWord: paymentinword,
+        PaymentInNo: paymentinno,
+    };
+
+    alert("hii");
+    $.ajax({
+        url: "/Pavati/SavePavati",
+        method: "Post",
+        data: JSON.stringify(model),
+        contentType: "application/json;charset=utf-8",
+        datatype: "json",
+
+        success: function (response) {
+            console.log(response.message);
+            alert("Successfull");
+
+            /*detail(reponse.message);*/
+        }
+    })
+};
+
+var getMainPavatiList = function () {
+    $.ajax({
+        url: "/Pavati/GetPavatiList",
+        method: "Post",
+        //data: JSON.stringify(model),
+        contentType: "application/json;charset=utf-8",
+        datatype: "json",
+        async: false,
+        success: function (response) {
+            var html = "";
+            $("#Tbl_Pavati tbody").empty();
+            $.each(response.model, function (index, elementValue) {
+                html += "<tr><td>" + elementValue.Id + "</td><td>" + elementValue.Date + "</td><td>" + elementValue.Name + "</td><td>" + elementValue.Mobileno + "</td><td>" + elementValue.PaymentInWord + "</td><td>" + elementValue.PaymentInNo + "</td><tr>";
+            });
+            $("#Tbl_Pavati").append(html);
+        }
+    });
+}
+
+var detail = function (id) {
+    var model = { ID: id };
+    
+    $.ajax({
+        url: "/Pavati/GetEditData ",
+        method: "post",
+        data: JSON.stringify(model),
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        async: false,
+        success: function (response) {
+            $("#txtId").val(response.model.id);
+            $("#txtDate").val(response.model.date);
+            $("#txtnamesame").val(response.model.name);
+            $("#txtmobilenosame").val(response.model.mobileno);
+            $("#txtpaymentinwordsame").val(response.model.paymentinword);
+            $("#txtpaymentinnosame").val(response.model.paymentinno);
+        }
+    });
+}
+
+
---
+++ Pingladevi/Controllers/Pavati/PavatiController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pingladevi.Models;
+
+
+namespace Pingladevi.Controllers.Pavati
+{
+    public class PavatiController : Controller
+    {
+        //GET:Pavati
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //public ActionResult PavatiList()
+        //{
+        //        var model = new PavatiModel().GetPavatiList();
+        //        return View("..//Pavati//PavatiList", model);            
+        //}
+        public ActionResult SavePavati(PavatiModel model)
+        {
+            try
+            {
+                return Json(new { message = (new PavatiModel().SavePavati(model)) }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception Ex)
+            {
+                return Json(new { model = Ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        //public ActionResult GetPavatiList()
+        //{
+        //    try
+        //    {
+        //         return Json(new { model = new PavatiModel().GetPavatiList() }, JsonRequestBehavior.AllowGet);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return Json(new { ex.Message }, JsonRequestBehavior.AllowGet);
+        //    }
+        //}
+
+        public ActionResult GetDetailData(int Id)
+        {
+            try
+            {
+                return Json(new { model = (new PavatiModel().DetailData(Id)) }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception Ex)
+            {
+                return Json(new { Ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
+
+
+
+
